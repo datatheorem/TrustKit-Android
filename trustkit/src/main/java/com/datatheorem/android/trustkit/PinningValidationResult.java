d81a96832d6b8e83b878f95ec18130331b645a4c
Alban Diquet <ad@sourcetheorem.com>
2016-09-22 20:15:28 -0700

    Document PinningValidationResult

--- trustkit/src/main/java/com/datatheorem/android/trustkit/PinningValidationResult.java
+++ trustkit/src/main/java/com/datatheorem/android/trustkit/PinningValidationResult.java
@@ -1,11 +1,22 @@
 package com.datatheorem.android.trustkit;
 
-// TODO(ad): Document this
+
 public enum PinningValidationResult {
+    // The server trust was successfully evaluated and contained at least one of the configured pins
     SUCCESS,
+
+    // The server trust was successfully evaluated but did not contain any of the configured pins
     FAILED,
+
+    // The server trust's evaluation failed: the server's certificate chain is not trusted
     FAILED_CERTIFICATE_CHAIN_NOT_TRUSTED,
+
+    // Not used on Android
     ERROR_INVALID_PARAMETERS,
+
+    // Not used on Android
     FAILED_USER_DEFINED_TRUST_ANCHOR,
-    ERROR_COULD_NOT_GENERATE_SPKI_HASH // Not used in TrustKit Android
+
+    // Not used on Android
+    ERROR_COULD_NOT_GENERATE_SPKI_HASH
 }
