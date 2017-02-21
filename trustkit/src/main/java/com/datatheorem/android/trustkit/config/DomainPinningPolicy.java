d224cc8c5928dfb93ffd7ee1a9ca4d9ae75ebd8b
Jordan <jb@sourcetheorem.com>
2017-02-21 17:46:40 +0100

    add check to verify if an empty pin-set is provided with enforcePinning set to true in the trustkit-config node

--- trustkit/src/main/java/com/datatheorem/android/trustkit/config/DomainPinningPolicy.java
+++ trustkit/src/main/java/com/datatheorem/android/trustkit/config/DomainPinningPolicy.java
@@ -48,10 +48,18 @@ public final class DomainPinningPolicy {
         }
         this.hostname = hostname.trim();
 
+        // Check if the configuration has a empty pin-set and still would enforce pinning
+        // TrustKit should not work if the configuration contains both (opposite behaviors)
+        if (publicKeyHashStrList.isEmpty() && shouldEnforcePinning) {
+            throw new ConfigurationException("An empty pin-set was supplied "+
+              "for domain " + this.hostname + " with the enforcePinning set to true. " +
+              "An empty pin-set disables pinning and can't be use with enforcePinning set to true.");
+        }
+
         // Check if the configuration has at least two pins (including a backup pin)
         // TrustKit should not work if the configuration contains only one pin
         // more info (https://tools.ietf.org/html/rfc7469#page-21)
-        if (publicKeyHashStrList.size() < 2) {
+        if (publicKeyHashStrList.size() < 2 && shouldEnforcePinning) {
             throw new ConfigurationException("Less than two pins were supplied "+
                     "for domain " + this.hostname + ". This might " +
                     "brick your App; please review the Getting Started guide in " +
