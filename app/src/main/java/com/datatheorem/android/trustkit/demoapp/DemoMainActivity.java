74c67938c66e95eabc1a65d22728a71f52f2ef42
jobot0 <me@jobo.to>
2019-06-21 13:50:02 +0200

    Update DemoApp to AndroidX

--- app/src/main/java/com/datatheorem/android/trustkit/demoapp/DemoMainActivity.java
+++ app/src/main/java/com/datatheorem/android/trustkit/demoapp/DemoMainActivity.java
@@ -3,9 +3,9 @@ package com.datatheorem.android.trustkit.demoapp;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,15 +25,16 @@ import javax.net.ssl.HttpsURLConnection;
 public class DemoMainActivity extends AppCompatActivity {
 
     protected static final String DEBUG_TAG = "TrustKit-Demo";
-    private static final PinningFailureReportBroadcastReceiver pinningFailureReportBroadcastReceiver = new PinningFailureReportBroadcastReceiver();
+    private static final PinningFailureReportBroadcastReceiver pinningFailureReportBroadcastReceiver
+        = new PinningFailureReportBroadcastReceiver();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        TextView textView = (TextView) findViewById(R.id.textview);
+        TextView textView = findViewById(R.id.textview);
 
         // Initialize TrustKit with the default path for the Network Security Configuration which is
         // res/xml/network_security_config.xml
